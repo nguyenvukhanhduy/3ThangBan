1f75575c63b57276f98407a7090d68829453f349
Nguyen Quoc Hung <hung.187pm06593>
2020-12-30 15:26:09 +0700

    add btn CheckNewOrder and Logout Admin

--- app/src/main/java/com/example/shoes/AdminCategoryActivity.java
+++ app/src/main/java/com/example/shoes/AdminCategoryActivity.java
@@ -6,16 +6,39 @@ import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class AdminCategoryActivity extends AppCompatActivity {
     private ImageView shoes;
+    private Button LogoutBtn,CheckOrdersBtn;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_category);
+        
+        LogoutBtn=(Button)findViewById(R.id.admin_logout_btn);
+        CheckOrdersBtn=(Button)findViewById(R.id.check_orders_btn);
         shoes=(ImageView)findViewById(R.id.shoes);
+
+        LogoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminCategoryActivity.this,MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+        CheckOrdersBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminCategoryActivity.this,AdminNewOrdersActivity.class);
+                startActivity(intent);
+            }
+        });
 
         shoes.setOnClickListener(new View.OnClickListener() {
             @Override
